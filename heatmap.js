fda4fdbb1f2cccd79c74df574afb2e4b1ed3194a
Blanca Azucena López Garduño <blanca.lopez@data4.mx>
2018-03-21 13:07:27 -0600

    Add missing semicolons (beautify)

--- heatmap.js
+++ heatmap.js
@@ -21,7 +21,7 @@ export default function (config, helper) {
       .html(vm._config.tip || function (d) {
         return vm.utils.format(d.value);
       });
-  }
+  };
 
   //-------------------------------
   //User config functions
@@ -29,25 +29,25 @@ export default function (config, helper) {
     var vm = this;
     vm._config.x = column;
     return vm;
-  }
+  };
 
   Heatmap.y = function (column) {
     var vm = this;
     vm._config.y = column;
     return vm;
-  }
+  };
 
   Heatmap.fill = function (column) {
     var vm = this;
     vm._config.fill = column;
     return vm;
-  }
+  };
 
   Heatmap.colors = function (colors) {
     var vm = this;
     vm._config.colors = colors;
     return vm;
-  }
+  };
 
   /**
    * Personalize border radius (rx, ry) for each rect
@@ -57,20 +57,20 @@ export default function (config, helper) {
     var vm = this;
     vm._config.borderRadius = radius;
     return vm;
-  }
+  };
 
   Heatmap.sortBy = function (sortBy) {
     var vm = this;
     vm._config.sortBy = sortBy;
     return vm;
-  }
+  };
 
   Heatmap.tip = function (tip) {
     var vm = this;
     vm._config.tip = tip;
     vm._tip.html(vm._config.tip);
     return vm;
-  }
+  };
 
   //-------------------------------
   //Triggered by the chart.js;
@@ -124,47 +124,47 @@ export default function (config, helper) {
       return m;
     });
     return vm;
-  }
+  };
 
   Heatmap.scales = function () {
     var vm = this;
     return vm;
-  }
+  };
 
   Heatmap.draw = function () {
     var vm = this;
 
     //Call the tip
-    vm.chart.svg().call(vm._tip)
+    vm.chart.svg().call(vm._tip);
 
-    vm._yLabels = vm.chart.svg().selectAll(".yLabels")
+    vm._yLabels = vm.chart.svg().selectAll('.yLabels')
       .data(vm._config.yCategories)
-      .enter().append("text")
+      .enter().append('text')
       .text(function (d) {
         return d;
       })
-      .attr("x", 0)
-      .attr("y", function (d, i) {
+      .attr('x', 0)
+      .attr('y', function (d, i) {
         return i * vm._gridHeight;
       })
-      .style("text-anchor", "end")
-      .attr("transform", "translate(-6," + vm._gridHeight / 1.5 + ")")
-      .attr("class", "yLabels");
-    //.attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
+      .style('text-anchor', 'end')
+      .attr('transform', 'translate(-6,' + vm._gridHeight / 1.5 + ')')
+      .attr('class', 'yLabels');
+    //.attr('class', function (d, i) { return ((i >= 0 && i <= 4) ? 'dayLabel mono axis axis-workweek' : 'dayLabel mono axis'); });
 
-    vm._xLabels = vm.chart.svg().selectAll(".xLabels")
+    vm._xLabels = vm.chart.svg().selectAll('.xLabels')
       .data(vm._config.xCategories)
-      .enter().append("text")
+      .enter().append('text')
       .text(function (d) {
         return d;
       })
-      .attr("x", function (d, i) {
+      .attr('x', function (d, i) {
         return i * vm._gridWidth;
       })
-      .attr("y", vm._config.yCategories.length * vm._gridHeight + 25)
-      .style("text-anchor", "middle")
-      .attr("transform", "translate(" + vm._gridWidth / 2 + ", -6)")
-      .attr("class", "xLabels mono axis");
+      .attr('y', vm._config.yCategories.length * vm._gridHeight + 25)
+      .style('text-anchor', 'middle')
+      .attr('transform', 'translate(' + vm._gridWidth / 2 + ', -6)')
+      .attr('class', 'xLabels mono axis');
 
 
     var colorScale = d3.scaleQuantile()
@@ -173,29 +173,29 @@ export default function (config, helper) {
       })])
       .range(vm._config.colors);
 
-    var cards = vm.chart.svg().selectAll(".grid-cell")
+    var cards = vm.chart.svg().selectAll('.grid-cell')
       .data(vm._data, function (d) {
         return d.y + ':' + d.x;
       });
 
-    cards.enter().append("rect")
-      .attr("x", function (d) {
+    cards.enter().append('rect')
+      .attr('x', function (d) {
         return (vm._config.xCategories.indexOf(String(d.x))) * vm._gridWidth;
       })
-      .attr("y", function (d) {
+      .attr('y', function (d) {
         return (vm._config.yCategories.indexOf(String(d.y))) * vm._gridHeight;
       })
-      .attr("rx", vm._config.borderRadius || 5)
-      .attr("ry", vm._config.borderRadius || 5)
+      .attr('rx', vm._config.borderRadius || 5)
+      .attr('ry', vm._config.borderRadius || 5)
 
-      .attr("class", "grid-cell")
-      .style("stroke", '#fff')
-      .style("stroke-width", '3px')
-      .attr("id", function (d) {
+      .attr('class', 'grid-cell')
+      .style('stroke', '#fff')
+      .style('stroke-width', '3px')
+      .attr('id', function (d) {
         return 'x' + d.x + 'y' + d.y;
       })
-      .attr("width", vm._gridWidth)
-      .attr("height", vm._gridHeight)
+      .attr('width', vm._gridWidth)
+      .attr('height', vm._gridHeight)
       .on('mouseover', function (d, i) {
         vm._tip.show(d, d3.select(this).node());
         if (vm._config.hasOwnProperty('mouseover')) {
@@ -208,41 +208,42 @@ export default function (config, helper) {
           vm._config.mouseout.call(this, d, i);
         }
       })
-      .on("click", function (d, i) {
+      .on('click', function (d, i) {
         if (vm._config.hasOwnProperty('onclick')) {
           vm._config.onclick.call(this, d, i);
         }
       })
-      .style("fill", vm._config.colors[0])
+      .style('fill', vm._config.colors[0])
       .transition()
       .duration(3000)
       .ease(d3.easeLinear)
-      .style("fill", function (d) {
+      .style('fill', function (d) {
         return colorScale(d.value);
       });
     /*
-      var legend = vm.chart.svg().selectAll(".legend")
+      var legend = vm.chart.svg().selectAll('.legend')
           .data([0].concat(colorScale.quantiles()), function(d) { return d; });
 
-      var lgroup = legend.enter().append("g")
-          .attr("class", "legend");
+      var lgroup = legend.enter().append('g')
+          .attr('class', 'legend');
 
-      lgroup.append("rect")
-          .attr("x", function(d, i) {  return vm._legendElementWidth * i; })
-          .attr("y", vm._config.size.height - vm._config.size.margin.bottom*2)
-          .attr("width", vm._legendElementWidth)
-          .attr("height", vm._gridWidth / 2)
-          .style("fill", function(d, i) { return vm._config.colors[i]; });
+      lgroup.append('rect')
+          .attr('x', function(d, i) {  return vm._legendElementWidth * i; })
+          .attr('y', vm._config.size.height - vm._config.size.margin.bottom*2)
+          .attr('width', vm._legendElementWidth)
+          .attr('height', vm._gridWidth / 2)
+          .style('fill', function(d, i) { return vm._config.colors[i]; });
 
-      lgroup.append("text")
-          .attr("class", "mono")
-          .text(function(d) { return "≥ " + Math.round(d); })
-          .attr("x", function(d, i) { return vm._legendElementWidth * i; })
-          .attr("y", vm._config.size.height - vm._config.size.margin.bottom*2 + vm._gridWidth);
+      lgroup.append('text')
+          .attr('class', 'mono')
+          .text(function(d) { return '≥ ' + Math.round(d); })
+          .attr('x', function(d, i) { return vm._legendElementWidth * i; })
+          .attr('y', vm._config.size.height - vm._config.size.margin.bottom*2 + vm._gridWidth);
 
       legend.exit().remove();*/
     return vm;
-  }
+  };
+
   Heatmap.init(config);
 
   return Heatmap;
